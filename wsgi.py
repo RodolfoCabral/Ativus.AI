bc4e982b816d1556b719714a4fdc22eaba1df350
Rodolfo Cabral <rodolfocabral@outlook.com.br>
2025-05-30 14:53:57 -0300

    Atualização

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Adiciona o diretório atual ao path do Python
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from src import create_app
 
 app = create_app()
