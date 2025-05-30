30ae549c555b97415856bdf78d0518d5063243c9
Rodolfo Cabral <rodolfocabral@outlook.com.br>
2025-05-30 15:14:35 -0300

    Atualização

--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
-from flask import Flask
+from src import create_app
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Aplicação funcionando!"
+app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
