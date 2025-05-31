07abb4581b54979aa9af7b1f0c48f0472b09d7f3
Rodolfo Cabral <rodolfocabral@outlook.com.br>
2025-05-31 00:05:25 -0300

    Retirando a base de dados para testar conexão com heroku

--- src/__init__.py
+++ src/__init__.py
@@ -9,7 +9,9 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 # Inicialização das extensões
+'''
 db = SQLAlchemy()
+'''
 login_manager = LoginManager()
 mail = Mail()
 
@@ -34,7 +36,9 @@ def create_app():
     app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', '')
     
     # Inicialização das extensões com o app
+    '''
     db.init_app(app)
+    '''
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
     login_manager.login_message = 'Por favor, faça login para acessar esta página.'
@@ -58,7 +62,9 @@ def create_app():
     
     # Criação das tabelas do banco de dados
     with app.app_context():
+        '''
         db.create_all()
+        '''
         
         # Criação do usuário administrador se não existir
         admin_user = User.query.filter_by(email='rodolfocabral@outlook.com.br').first()
@@ -69,7 +75,9 @@ def create_app():
                 is_admin=True
             )
             admin_user.set_password('101002Rm#')
+            '''
             db.session.add(admin_user)
             db.session.commit()
+            '''
     
     return app
